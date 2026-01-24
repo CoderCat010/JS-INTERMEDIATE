95035210728bfe5a35925803e79fee0b3c02ae89
CoderCat010 <nusraumme@gmail.com>
2026-01-24 10:26:49 +0600

    added practice-5

--- js-dom-practice-1.js/script.js
+++ js-dom-practice-1.js/script.js
@@ -1,13 +1,22 @@
 console.log(document.body);
 
+//------tag name
 const listCollection = document.getElementsByTagName('li');
 
 // console.log(listCollection);
 // for(const li of listCollection){
 //     console.log(li.innerText); 
 // }
+
+//------id name
 const callById = document.getElementById('h1-tag');
 
-console.log(callById);
-console.log(callById.innerText);
+// console.log(callById);
+// console.log(callById.innerText);
+
+
+//------class name
+const items = document.getElementsByClassName('list-items');
+console.log(items[0].innerText);
+
 
