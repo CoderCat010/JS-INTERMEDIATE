b1df8c345022410a5d26292a31b0e66ffe367cd0
CoderCat010 <nusraumme@gmail.com>
2026-01-24 12:03:44 +0600

    added practice-10

---
+++ js-dom-practice-1.js/script.js
@@ -0,0 +1,8 @@
+// get elementsById()
+const byId = document.getElementById('heading-1');
+console.log(byId); 
+
+
+// getElementsByClassName()
+const byClassName = document.getElementsByClassName('heading-2');
+console.log(byClassName);
