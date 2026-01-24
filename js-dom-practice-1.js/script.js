23778250aa9034b4fc05c2adfde4fa8fe35a269d
CoderCat010 <nusraumme@gmail.com>
2026-01-24 10:32:20 +0600

    added practice-7

--- js-dom-practice-1.js/script.js
+++ js-dom-practice-1.js/script.js
@@ -18,9 +18,13 @@ const callById = document.getElementById('h1-tag');
 //------class name
 const items = document.getElementsByClassName('list-items');
 // console.log(items[0].innerText);
-for(let elements of items){
-    console.log(elements.innerText);
-    
-}
+// for(let elements of items){
+//     console.log(elements.innerText);
+// }
 
+
+//-------changing inner text
+const changing = document.getElementById('h2-tag');
+changing.innerText = 'changed!';
+console.log(changing);
 
