bdcf542c8c8a9ab4f75b7c84d4291936ed172d22
CoderCat010 <nusraumme@gmail.com>
2026-01-26 05:56:31 +0600

    added practice-2

---
+++ js-dom-practice-2/style-property.js
@@ -0,0 +1,9 @@
+const boxContent = document.querySelectorAll('.box');
+for(let value of boxContent){
+    value.style.color = 'red'
+    value.style.width = '300px'
+}
+const parentDiv = document.getElementsByTagName('header');
+for(let values of parentDiv){
+    values.style.display = 'flex';
+}
