455b30de5b96a4db8cb3a6c921d4511ffd5f6e90
CoderCat010 <nusraumme@gmail.com>
2026-01-25 19:49:40 +0600

    added practice-9

--- js-dom-practice-2/textContent.js
+++ js-dom-practice-2/textContent.js
@@ -1,5 +1,9 @@
 //textContent 
-const element = document.querySelector('.heading-2');
-console.log(element.textContent);
+// const element = document.querySelector('.heading-2');
+// console.log(element.textContent);
+
+const element = document.querySelector('.heading-1 span');
+element.textContent = '';
+console.log(element);
 
 
