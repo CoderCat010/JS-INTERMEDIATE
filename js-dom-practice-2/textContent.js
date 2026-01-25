798a25f4a2f574fae64c825e94a154dcc90e2736
CoderCat010 <nusraumme@gmail.com>
2026-01-25 20:03:38 +0600

    added files

--- js-dom-practice-2/textContent.js
+++ js-dom-practice-2/textContent.js
@@ -2,8 +2,8 @@
 // const element = document.querySelector('.heading-2');
 // console.log(element.textContent);
 
-const element = document.querySelector('.heading-1 span');
-element.textContent = '';
-console.log(element);
+// const element = document.querySelector('.heading-1 span');
+// element.textContent = '';
+// console.log(element);
 
 
