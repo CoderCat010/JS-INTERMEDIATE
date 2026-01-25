8e3e2a08dd1b8d85115f69cb290cb346fd327719
CoderCat010 <nusraumme@gmail.com>
2026-01-25 18:15:09 +0600

    added practice-3

--- js-dom-practice-2/inner-html-practice.js
+++ js-dom-practice-2/inner-html-practice.js
@@ -1,4 +1,11 @@
 //innerHTML()
-const innerHTML = document.getElementById('heading-1').innerHTML;
+// const innerHTML = document.getElementById('heading-1').innerHTML;
 // console.log(innerHTML);
 
+
+//changing content by html
+// const innerHTML = document.getElementById('heading-1 span').innerHTML = 'sodesh ';
+// console.log(innerHTML);
+// innerHTML diyei span er content change kora
+const innerHTML = document.querySelector('.heading-1 span').innerHTML = 'sodesh ';
+console.log(innerHTML); // Output: 'sodesh '
