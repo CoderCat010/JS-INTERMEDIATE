a99d7e58e58aff1b7f3e13479c00525d75e9121a
CoderCat010 <nusraumme@gmail.com>
2026-01-25 18:40:37 +0600

    added practice-5

--- js-dom-practice-2/inner-html-practice.js
+++ js-dom-practice-2/inner-html-practice.js
@@ -3,6 +3,14 @@
 // console.log(innerHTML);
 
 
-//changing content by html
+//changing content by innerhtml
 // const innerHTML = document.querySelector('.heading-1 span').innerHTML = 'sodesh ';
 // console.log(innerHTML); 
+
+
+//adding content by innerhtml
+// const innerHTML = document.querySelector('.heading-1').innerHTML += ' sodesh '; // at last
+
+// const innerHTMLContent = document.querySelector('.heading-2');
+// innerHTMLContent.innerHTML = ' sodesh '+ innerHTMLContent.innerHTML;
+// console.log(innerHTMLContent); // at first
