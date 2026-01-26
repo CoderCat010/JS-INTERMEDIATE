25075e7b7c0e236d95da484df2b757f4a199f76a
CoderCat010 <nusraumme@gmail.com>
2026-01-26 07:22:15 +0600

    added practice-5

--- js-dom-practice-2/style-cssText.js
+++ js-dom-practice-2/style-cssText.js
@@ -1,4 +1,4 @@
-const boxs = document.querySelectorAll('.box');
-for(let values of boxs){
-    values.style.cssText = 'color: red; width: 300px; height: 200px; border: 3px solid green';
-}
+// const boxs = document.querySelectorAll('.box');
+// for(let values of boxs){
+//     values.style.cssText = 'color: red; width: 300px; height: 200px; border: 3px solid green';
+// }
