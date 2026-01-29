d7d54cbe0b77d25b54dc11b8c7493b144a3f762c
CoderCat010 <nusraumme@gmail.com>
2026-01-29 16:58:52 +0600

    added practice-17

--- js-dom-practice-2/apendChild.js
+++ js-dom-practice-2/apendChild.js
@@ -2,3 +2,8 @@
 const newItems = document.createElement('li');
 newItems.textContent = 'lorem lorem lorem';
 console.log(newItems);
+
+//-----> adding new element in the last 
+const lastElm = document.getElementById('myList');
+lastElm.appendChild(newItems);
+console.log(lastElm);
