a055812b08631066d582287b3c200667bfac71ea
CoderCat010 <nusraumme@gmail.com>
2026-01-29 18:33:26 +0600

    added practice-23

--- js-dom-practice-2/insert-before.js
+++ js-dom-practice-2/insert-before.js
@@ -1,3 +1,4 @@
+//-----> Insert Before
 // const container = document.getElementsByTagName('ul')[0];
 // const parentChild = document.getElementById('insertElm');
 
@@ -5,3 +6,13 @@
 // insertNewElm.textContent = 'modified by js';
 
 // container.insertBefore(insertNewElm, parentChild);
+
+ 
+//-----> Insert After
+const Parentcontainer = document.getElementsByTagName('div')[0];
+const parentChildList = document.getElementsByClassName('paragraph-2')[0];
+
+const insertNewElm = document.createElement('p');
+insertNewElm.textContent = 'modified by js after';
+
+Parentcontainer.insertBefore(insertNewElm, parentChildList.nextSibling);
