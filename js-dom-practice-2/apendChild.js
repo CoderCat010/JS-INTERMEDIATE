718fa2c474eca0d63f54284df34254b64dbacbda
CoderCat010 <nusraumme@gmail.com>
2026-01-29 17:36:11 +0600

    added practice-20

--- js-dom-practice-2/apendChild.js
+++ js-dom-practice-2/apendChild.js
@@ -1,12 +1,12 @@
 //-----> create new elements
-const newItems = document.createElement('li');
-newItems.textContent = 'lorem lorem lorem';
+// const newItems = document.createElement('li');
+// newItems.textContent = 'lorem lorem lorem';
 // console.log(newItems);
 
 
 //-----> adding new element in the last 
-const lastElm = document.getElementById('myList');
-lastElm.appendChild(newItems);
+// const lastElm = document.getElementById('myList');
+// lastElm.appendChild(newItems);
 // console.log(lastElm);
 
 
@@ -28,3 +28,4 @@ lastElm.appendChild(newItems);
 //     newParagraph.textContent = 'pew pew peww' + i;
 //     items.appendChild(newParagraph);
 // }
+
