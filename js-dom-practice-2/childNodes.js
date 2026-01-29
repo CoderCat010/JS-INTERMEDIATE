495ad1ff02589c84d9cf2f0f016fcc462893904a
CoderCat010 <nusraumme@gmail.com>
2026-01-29 16:34:17 +0600

    added practice-15

--- js-dom-practice-2/childNodes.js
+++ js-dom-practice-2/childNodes.js
@@ -2,3 +2,7 @@ const nodeList = document.getElementById('list');
 console.log(nodeList.childNodes);
 console.log(nodeList.firstChild);
 console.log(nodeList.childNodes[3]);
+console.log(nodeList.nextSibling);
+console.log(nodeList.previousSibling);
+
+
