c0bdd95db664f2477be0dd83b3c6b330ed8a165d
CoderCat010 <nusraumme@gmail.com>
2026-01-29 17:23:21 +0600

    added practice-18

--- js-dom-practice-2/apendChild.js
+++ js-dom-practice-2/apendChild.js
@@ -1,9 +1,21 @@
 //-----> create new elements
 const newItems = document.createElement('li');
 newItems.textContent = 'lorem lorem lorem';
-console.log(newItems);
+// console.log(newItems);
+
 
 //-----> adding new element in the last 
 const lastElm = document.getElementById('myList');
 lastElm.appendChild(newItems);
-console.log(lastElm);
+// console.log(lastElm);
+
+
+//-----> adding multiple elements
+const items = document.getElementsByClassName('paraContainer');
+const container = items[2];
+
+for(let i = 3; i <= 5; i++){
+    const newParagraph = document.createElement('p');
+    newParagraph.textContent = 'pew pew peww' + i;
+    container.appendChild(newParagraph);
+}
