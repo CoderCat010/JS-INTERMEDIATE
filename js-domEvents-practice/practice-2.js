ec69c0a02b81a0b5d46e801a72623be51b8a5700
CoderCat010 <nusraumme@gmail.com>
2026-01-31 18:50:43 +0600

    added practice-2

---
+++ js-domEvents-practice/practice-2.js
@@ -0,0 +1,5 @@
+document.getElementById('btn').addEventListener('click', function(){
+    const para = document.getElementById('text');
+    // para.style.display = 'none';
+    para.style.visibility ='hidden'
+})
