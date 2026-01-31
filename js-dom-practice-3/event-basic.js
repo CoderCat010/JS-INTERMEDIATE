0c0895d1669ba3885dc94cadb81a760f99261fec
CoderCat010 <nusraumme@gmail.com>
2026-01-31 06:49:33 +0600

    added practice-6

--- js-dom-practice-3/event-basic.js
+++ js-dom-practice-3/event-basic.js
@@ -3,12 +3,14 @@
 //     document.body.style.backgroundColor = 'red';
 // }
 
+
 // yellow buttton
 // const btnYellow = document.getElementById('btn-1');
 
 // btnYellow.onclick = function makeYellow(){
 //     document.body.style.backgroundColor = 'yellow';
 // }
+
 
 // green button
 // const btnGreen = document.getElementById('btn-2');
@@ -18,7 +20,16 @@
 // }
 // btnGreen.onclick = makeGreen;
 
+
 // purple button 
 // document.getElementById('btn-3').addEventListener('click', function(){
 //     document.body.style.backgroundColor = 'purple';
 // })
+
+
+// changing inner text 
+document.getElementById('btn-4').addEventListener('click', function(){
+    const userInfo = document.getElementById('user-info');
+    userInfo.textContent = 'user logged in successfully!'
+})
+
